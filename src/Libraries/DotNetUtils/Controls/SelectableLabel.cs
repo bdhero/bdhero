25b8bbfb5e2ddf9e47abcab5f9702bf3d730da09
Andrew C. Dvorak <acdvorak@gmail.com>
2013-11-15 22:01:28 -0600

    Enable CTRL + A to select all text in SelectableLabel.cs

--- src/Libraries/DotNetUtils/Controls/SelectableLabel.cs
+++ src/Libraries/DotNetUtils/Controls/SelectableLabel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using DotNetUtils.Extensions;
 
 namespace DotNetUtils.Controls
 {
@@ -12,6 +13,7 @@ namespace DotNetUtils.Controls
         {
             ReadOnly = true;
             BorderStyle = BorderStyle.None;
+            this.EnableSelectAll();
         }
 
         protected override void OnParentBackColorChanged(EventArgs e)
