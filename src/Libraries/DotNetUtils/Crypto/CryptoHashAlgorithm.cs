bca5d0abe3e03769bb6088a0fd39f7e04af93005
Andrew C. Dvorak <acdvorak@gmail.com>
2013-11-24 19:51:49 -0600

    Minor - Added XMLDoc comments to CryptoHashAlgorithm.cs

--- src/Libraries/DotNetUtils/Crypto/CryptoHashAlgorithm.cs
+++ src/Libraries/DotNetUtils/Crypto/CryptoHashAlgorithm.cs
@@ -1,27 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 
 namespace DotNetUtils.Crypto
 {
+    /// <summary>
+    ///     Abstract base class for crypographic hashing algorithms.
+    ///     Provides convenience methods for computing hexadecimal hashes (0-9, a-f) from strings, files, streams,
+    ///     and byte arrays.
+    /// </summary>
     public abstract class CryptoHashAlgorithm
     {
-        public static bool LowerCase = false;
+        /// <summary>
+        ///     Gets or sets whether hexadecimal hash values should be uppercase (<c>true</c>) or lowercase (<c>false</c>).
+        ///     The default is <c>true</c>.
+        /// </summary>
+        public bool UpperCase = true;
 
         #region Abstract members
 
+        /// <summary>
+        ///     Provides the implementation for the hashing algorithm.
+        /// </summary>
+        /// <param name="buffer">Input data to be hashed.</param>
+        /// <returns>The computed hash value of <paramref name="buffer"/>.</returns>
         protected abstract byte[] ComputeImpl(byte[] buffer);
 
         /// <summary>
-        /// Human-friendly name of the algorithm.  E.G., "SHA-1".
+        ///     Human-friendly name of the algorithm.  E.G., "SHA-1".
         /// </summary>
         public abstract string HumanName { get; }
 
         /// <summary>
-        /// Lowercase alphanumerics only (e.g., "sha1").
+        ///     Lowercase alphanumerics only (e.g., "sha1").
         /// </summary>
         public abstract string MachineName { get; }
 
@@ -29,37 +40,61 @@ namespace DotNetUtils.Crypto
 
         #region Concrete members
 
+        /// <summary>
+        ///     Computes the crypographic hash of the given <paramref name="text"/>.
+        /// </summary>
+        /// <param name="text">Input data to be hashed.</param>
+        /// <returns>The computed hash value of <paramref name="text"/>.</returns>
         public string ComputeText(string text)
         {
             return ComputeBytes(Encoding.UTF8.GetBytes(text));
         }
 
+        /// <summary>
+        ///     Computes the crypographic hash of <paramref name="path"/>'s contents.
+        /// </summary>
+        /// <param name="path">Path to a file.</param>
+        /// <returns>The computed hash value of <paramref name="path"/>'s contents.</returns>
         public string ComputeFile(string path)
         {
             return ComputeBytes(File.ReadAllBytes(path));
         }
 
+        /// <summary>
+        ///     Computes the crypographic hash of <paramref name="stream"/>'s contents.
+        /// </summary>
+        /// <param name="stream">Input data stream to hash.</param>
+        /// <returns>The computed hash value of <paramref name="stream"/>'s contents.</returns>
         public string ComputeStream(Stream stream)
         {
             return ComputeBytes(FileUtils.ReadStream(stream));
         }
 
+        /// <summary>
+        ///     Computes the crypographic hash of <paramref name="buffer"/>'s contents.
+        /// </summary>
+        /// <param name="buffer">Input data to hash.</param>
+        /// <returns>The computed hash value of <paramref name="buffer"/>'s contents.</returns>
         public string ComputeBytes(byte[] buffer)
         {
+            var format = UpperCase ? "X2" : "x2";
             var hash = ComputeImpl(buffer);
             var sb = new StringBuilder();
-            for (var i = 0; i < hash.Length; i++)
+            foreach (byte b in hash)
             {
-                sb.Append(hash[i].ToString("X2"));
+                sb.Append(b.ToString(format));
             }
-            var str = sb.ToString();
-            return LowerCase ? str.ToLowerInvariant() : str;
+            return sb.ToString();
         }
 
         #endregion
 
         #region ToString
 
+        /// <summary>
+        ///     Returns a string that represents the current cryptographic hashing algorithm.
+        /// </summary>
+        /// <returns>A string that represents the current cryptographic hashing algorithm.</returns>
         public override string ToString()
         {
             return MachineName;
@@ -69,11 +104,28 @@ namespace DotNetUtils.Crypto
 
         #region Equality
 
+        /// <summary>
+        ///     Determines if the current algorithm and the <paramref name="other"/> algorithm represent the same
+        ///     cryptographic hashing algorithm.
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns>
+        ///     <c>true</c> if this algorithm represent the same cryptographic hashing algorithm as
+        ///     <paramref name="other"/>; otherwise <c>false</c>.
+        /// </returns>
         protected bool Equals(CryptoHashAlgorithm other)
         {
             return string.Equals(MachineName, other.MachineName);
         }
 
+        /// <summary>
+        ///     Determines if the current algorithm is equal to <paramref name="obj"/>.
+        /// </summary>
+        /// <param name="obj">Object to compare.</param>
+        /// <returns>
+        ///     <c>true</c> if <paramref name="obj"/> has the same runtime class and represents the same cryptographic
+        ///     hashing algorithm as this algorithm; otherwise <c>false</c>.
+        /// </returns>
         public override bool Equals(object obj)
         {
             if (ReferenceEquals(null, obj)) return false;
@@ -90,6 +142,9 @@ namespace DotNetUtils.Crypto
         #endregion
     }
 
+    /// <summary>
+    ///     Concrete implementation of <seealso cref="CryptoHashAlgorithm"/> that generates MD5 sums.
+    /// </summary>
     public class MD5Algorithm : CryptoHashAlgorithm
     {
         protected override byte[] ComputeImpl(byte[] buffer)
@@ -111,6 +166,9 @@ namespace DotNetUtils.Crypto
         }
     }
 
+    /// <summary>
+    ///     Concrete implementation of <seealso cref="CryptoHashAlgorithm"/> that generates SHA-1 sums.
+    /// </summary>
     public class SHA1Algorithm : CryptoHashAlgorithm
     {
         protected override byte[] ComputeImpl(byte[] buffer)
@@ -132,6 +190,9 @@ namespace DotNetUtils.Crypto
         }
     }
 
+    /// <summary>
+    ///     Concrete implementation of <seealso cref="CryptoHashAlgorithm"/> that generates SHA-256 sums.
+    /// </summary>
     public class SHA256Algorithm : CryptoHashAlgorithm
     {
         protected override byte[] ComputeImpl(byte[] buffer)
@@ -153,6 +214,9 @@ namespace DotNetUtils.Crypto
         }
     }
 
+    /// <summary>
+    ///     Concrete implementation of <seealso cref="CryptoHashAlgorithm"/> that generates SHA-512 sums.
+    /// </summary>
     public class SHA512Algorithm : CryptoHashAlgorithm
     {
         protected override byte[] ComputeImpl(byte[] buffer)
