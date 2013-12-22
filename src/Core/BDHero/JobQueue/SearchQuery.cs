595529826afab6373e43ae2d7ed1eedc0d88ad0a
Andrew C. Dvorak <acdvorak@gmail.com>
2013-12-21 22:06:17 -0600

    Added Equals() and GetHashCode() methods to SearchQuery.cs

--- src/Core/BDHero/JobQueue/SearchQuery.cs
+++ src/Core/BDHero/JobQueue/SearchQuery.cs
@@ -12,6 +12,39 @@ namespace BDHero.JobQueue
         public int? Year;
         public Language Language = Language.English;
 
+        protected bool Equals(SearchQuery other)
+        {
+            return string.Equals(Title, other.Title) && Year == other.Year && Equals(Language, other.Language);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+            return Equals((SearchQuery) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (Title != null ? Title.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ Year.GetHashCode();
+                hashCode = (hashCode * 397) ^ (Language != null ? Language.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+
         public override string ToString()
         {
             var iso639_2 = Language != null ? string.Format("{0}: ", Language.ISO_639_2) : "";
