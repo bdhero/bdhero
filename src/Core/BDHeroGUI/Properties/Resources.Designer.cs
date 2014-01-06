02e309debbfc51b5d6c00ba2ae1bbe843057cdaf
Andrew C. Dvorak <acdvorak@gmail.com>
2014-01-06 02:58:01 -0600

    Added network.png to BDHeroGUI/Resources

--- src/Core/BDHeroGUI/Properties/Resources.Designer.cs
+++ src/Core/BDHeroGUI/Properties/Resources.Designer.cs
@@ -244,6 +244,16 @@ namespace BDHeroGUI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap network {
+            get {
+                object obj = ResourceManager.GetObject("network", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap no_poster_w185 {
             get {
                 object obj = ResourceManager.GetObject("no_poster_w185", resourceCulture);
