f26f03dc1da537d1fadeb8086973d2f7ea3ef97e
Andrew C. Dvorak <acdvorak@gmail.com>
2014-01-06 20:01:12 -0600

    Added FormResizeWindow.cs

---
+++ src/Core/BDHeroGUI/Forms/FormResizeWindow.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace BDHeroGUI.Forms
+{
+    partial class FormResizeWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxWidth = new System.Windows.Forms.NumericUpDown();
+            this.textBoxHeight = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.textBoxWidth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textBoxHeight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Width:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(99, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Height:";
+            // 
+            // textBoxWidth
+            // 
+            this.textBoxWidth.Location = new System.Drawing.Point(12, 25);
+            this.textBoxWidth.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.textBoxWidth.Minimum = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            this.textBoxWidth.Name = "textBoxWidth";
+            this.textBoxWidth.Size = new System.Drawing.Size(66, 20);
+            this.textBoxWidth.TabIndex = 2;
+            this.textBoxWidth.Value = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            // 
+            // textBoxHeight
+            // 
+            this.textBoxHeight.Location = new System.Drawing.Point(102, 25);
+            this.textBoxHeight.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.textBoxHeight.Minimum = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            this.textBoxHeight.Name = "textBoxHeight";
+            this.textBoxHeight.Size = new System.Drawing.Size(66, 20);
+            this.textBoxHeight.TabIndex = 3;
+            this.textBoxHeight.Value = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(84, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(12, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "x";
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Location = new System.Drawing.Point(12, 51);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(75, 23);
+            this.buttonOk.TabIndex = 5;
+            this.buttonOk.Text = "&OK";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(93, 51);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 6;
+            this.buttonCancel.Text = "&Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // FormResizeWindow
+            // 
+            this.AcceptButton = this.buttonOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonCancel;
+            this.ClientSize = new System.Drawing.Size(180, 86);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxHeight);
+            this.Controls.Add(this.textBoxWidth);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormResizeWindow";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Resize Window";
+            ((System.ComponentModel.ISupportInitialize)(this.textBoxWidth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textBoxHeight)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown textBoxWidth;
+        private System.Windows.Forms.NumericUpDown textBoxHeight;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.Button buttonCancel;
+    }
+}
---
+++ src/Core/BDHeroGUI/Forms/FormResizeWindow.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BDHeroGUI.Forms
+{
+    public partial class FormResizeWindow : Form
+    {
+        private readonly Form _parentForm;
+        private readonly Size _originalSize;
+
+        public FormResizeWindow(Form parentForm)
+        {
+            InitializeComponent();
+
+            _parentForm = parentForm;
+            _originalSize = _parentForm.Size;
+
+            var minimumSize = parentForm.MinimumSize;
+            var maximumSize = parentForm.MaximumSize;
+
+            if (!minimumSize.IsEmpty)
+            {
+                textBoxWidth.Minimum = minimumSize.Width;
+                textBoxHeight.Minimum = minimumSize.Height;
+            }
+
+            if (!maximumSize.IsEmpty)
+            {
+                textBoxWidth.Maximum = maximumSize.Width;
+                textBoxHeight.Maximum = maximumSize.Height;
+            }
+
+            textBoxWidth.Value = _originalSize.Width;
+            textBoxHeight.Value = _originalSize.Height;
+
+            textBoxWidth.ValueChanged += OnChanged;
+            textBoxHeight.ValueChanged += OnChanged;
+            Closed += OnClosed;
+        }
+
+        private void OnChanged(object sender, EventArgs eventArgs)
+        {
+            _parentForm.Size = new Size((int)textBoxWidth.Value, (int)textBoxHeight.Value);
+        }
+
+        private void OnClosed(object sender, EventArgs eventArgs)
+        {
+            if (DialogResult == DialogResult.OK)
+                return;
+            _parentForm.Size = _originalSize;
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
