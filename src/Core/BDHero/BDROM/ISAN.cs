28f157757da4af990d4a6ed178787cae38d37f42
Andrew C. Dvorak <acdvorak@gmail.com>
2014-01-09 08:51:13 -0600

    Made Isan.IsSearchable more robust
    Previously, IsSearchable only checked Root against an array of known invalid roots.  It now checks every 4-digit segment individually.  If all segments are either "0000" or "FFFF", the ISAN is considered invalid and not searchable.

--- src/Core/BDHero/BDROM/ISAN.cs
+++ src/Core/BDHero/BDROM/ISAN.cs
@@ -78,7 +78,7 @@ namespace BDHero.BDROM
     /// TODO: Add language(s), alternate title(s), and additional information
     public class Isan
     {
-        private static readonly string[] InvalidRoots = {"000000000000", "FFFFFFFFFFFF"};
+        private static readonly string[] InvalidSegments = { "0000", "FFFF" };
 
         /// <summary>
         /// Matches any valid ISAN number <em>without dashes</em> at word boundaries.
@@ -156,7 +156,10 @@ namespace BDHero.BDROM
         /// <summary>
         /// Gets a value indicating whether the ISAN's <see cref="Root"/> is valid and searchable (i.e., is not all zeros or Fs).
         /// </summary>
-        public bool IsSearchable { get { return !InvalidRoots.Contains(Root); } }
+        public bool IsSearchable
+        {
+            get { return !NumberFormatted.Split('-').All(segment => InvalidSegments.Contains(segment)); }
+        }
 
         protected Isan(string n1, string n2, string n3, string n4, string n5, string n6)
         {
