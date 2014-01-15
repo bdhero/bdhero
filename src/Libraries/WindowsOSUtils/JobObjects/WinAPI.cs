e3abcbd798f4cbb2cd0a5e1b2eafb0a6c072bf41
Andrew C. Dvorak <acdvorak@gmail.com>
2014-01-15 11:48:26 -0600

    Added copyright and license statement to the top of all .cs files

--- src/Libraries/WindowsOSUtils/JobObjects/WinAPI.cs
+++ src/Libraries/WindowsOSUtils/JobObjects/WinAPI.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿// Copyright 2012, 2013, 2014 Andrew C. Dvorak
+//
+// This file is part of BDHero.
+//
+// BDHero is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// BDHero is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with BDHero.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
 using System.Runtime.InteropServices;
 
 // ReSharper disable UnusedMember.Global
