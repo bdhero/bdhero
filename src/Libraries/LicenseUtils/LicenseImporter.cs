a4ef9e21eb5076b27792beedf2680bc206aef9d7
Andrew C. Dvorak <acdvorak@gmail.com>
2014-02-02 16:01:15 -0600

    Minor - Removed unused references

--- src/Libraries/LicenseUtils/LicenseImporter.cs
+++ src/Libraries/LicenseUtils/LicenseImporter.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using System.Resources;
 using System.Text;
 using LicenseUtils.Properties;
 using MarkdownSharp;
