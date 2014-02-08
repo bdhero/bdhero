bf71e8308b72f5dcbfab3f6bfad4e6278f1ac477
Andrew C. Dvorak <acdvorak@gmail.com>
2014-02-08 17:45:31 -0600

    Added mail.png to DotNetUtils Resources

--- src/Libraries/DotNetUtils/Properties/Resources.Designer.cs
+++ src/Libraries/DotNetUtils/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace DotNetUtils.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap mail {
+            get {
+                object obj = ResourceManager.GetObject("mail", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap network {
             get {
                 object obj = ResourceManager.GetObject("network", resourceCulture);
