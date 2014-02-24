0dd9d542becf6ae5b6c301e85586700185205717
Andrew C. Dvorak <acdvorak@gmail.com>
2014-02-24 13:53:40 -0600

    Minor/ignore - Formatting

--- src/Core/BDHero/Utils/AppConstants.cs
+++ src/Core/BDHero/Utils/AppConstants.cs
@@ -21,16 +21,15 @@ using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using DotNetUtils;
 
 namespace BDHero.Utils
 {
     public static class AppConstants
     {
-        public const string ProjectHomepage = @"https://github.com/bdhero/bdhero";
-        public const string SourceCodeUrl = @"https://github.com/bdhero/bdhero";
-        public const string DocumentationUrl = @"https://github.com/bdhero/bdhero/wiki";
-        public const string BugReportUrl = @"https://github.com/bdhero/bdhero/issues/new";
+        public const string ProjectHomepage   = @"https://github.com/bdhero/bdhero";
+        public const string SourceCodeUrl     = @"https://github.com/bdhero/bdhero";
+        public const string DocumentationUrl  = @"https://github.com/bdhero/bdhero/wiki";
+        public const string BugReportUrl      = @"https://github.com/bdhero/bdhero/issues/new";
         public const string SuggestFeatureUrl = @"https://github.com/bdhero/bdhero/issues/new";
     }
 }
