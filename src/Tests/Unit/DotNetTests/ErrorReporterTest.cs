3fcdf11eed96fd1fc586449490d62d6879717473
Andrew C. Dvorak <acdvorak@gmail.com>
2014-02-24 19:53:51 -0600

    Minor/ignore - Removed unused usings

--- src/Tests/Unit/DotNetTests/ErrorReporterTest.cs
+++ src/Tests/Unit/DotNetTests/ErrorReporterTest.cs
@@ -1,13 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Net;
-using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
-using BDHero;
 using BDHero.ErrorReporting;
 using NUnit.Framework;
 
