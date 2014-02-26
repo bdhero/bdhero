f7dcdd023cd17d568ff9b1ba24cdfcb34c8549bf
Andrew C. Dvorak <acdvorak@gmail.com>
2014-02-26 08:44:58 -0600

    Mono compat in FormErrorReport

--- src/Core/BDHeroGUI/Forms/FormErrorReport.Designer.cs
+++ src/Core/BDHeroGUI/Forms/FormErrorReport.Designer.cs
@@ -28,56 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-            this.splitContainer1.Panel1.SuspendLayout();
-            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // splitContainer1
-            // 
-            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Name = "splitContainer1";
-            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
-            // 
-            // splitContainer1.Panel1
-            // 
-            this.splitContainer1.Panel1.Controls.Add(this.elementHost1);
-            this.splitContainer1.Size = new System.Drawing.Size(789, 650);
-            this.splitContainer1.SplitterDistance = 263;
-            this.splitContainer1.TabIndex = 0;
-            // 
-            // elementHost1
-            // 
-            this.elementHost1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.elementHost1.Location = new System.Drawing.Point(0, 0);
-            this.elementHost1.Name = "elementHost1";
-            this.elementHost1.Size = new System.Drawing.Size(789, 263);
-            this.elementHost1.TabIndex = 0;
-            this.elementHost1.Text = "elementHost1";
-            this.elementHost1.Child = null;
             // 
             // FormErrorReport
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(789, 650);
-            this.Controls.Add(this.splitContainer1);
             this.Name = "FormErrorReport";
             this.Text = "FormErrorReport";
-            this.splitContainer1.Panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.SplitContainer splitContainer1;
-        private System.Windows.Forms.Integration.ElementHost elementHost1;
 
     }
 }
--- src/Core/BDHeroGUI/Forms/FormErrorReport.cs
+++ src/Core/BDHeroGUI/Forms/FormErrorReport.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Windows.Forms.Integration;
 using System.Windows.Media;
 using ICSharpCode.AvalonEdit.Highlighting;
 
@@ -20,15 +21,28 @@ namespace BDHeroGUI.Forms
         {
             InitializeComponent();
 
-            InitWpfEditor();
-
-            InitWfEditor();
+            InitEditor();
         }
+
+#if __MonoCS__
+
+        /// <summary>
+        ///     Windows Forms editor
+        /// </summary>
+        private void InitEditor()
+        {
+            var wfEditor = new ICSharpCode.TextEditor.TextEditorControl();
+            wfEditor.Dock = DockStyle.Fill;
+            wfEditor.LoadFile(FilePath, true, true);
+            splitContainer1.Panel2.Controls.Add(wfEditor);
+        }
+
+#else
 
         /// <summary>
         ///     Windows Presentation Foundation editor
         /// </summary>
-        private void InitWpfEditor()
+        private void InitEditor()
         {
             var wpfEditor = new ICSharpCode.AvalonEdit.TextEditor();
             wpfEditor.Load(FilePath);
@@ -36,6 +50,8 @@ namespace BDHeroGUI.Forms
             // Font
             wpfEditor.FontFamily = new FontFamily("Consolas, Courier New, monospace");
             wpfEditor.FontSize = 14;
+
+            #region Options
 
             // Options
             wpfEditor.ShowLineNumbers = true;
@@ -49,21 +65,19 @@ namespace BDHeroGUI.Forms
             wpfEditor.Options.ShowSpaces = true;
             wpfEditor.Options.ShowTabs = true;
 
+            #endregion
+
             var highlightingManager = HighlightingManager.Instance;
             wpfEditor.SyntaxHighlighting = highlightingManager.GetDefinitionByExtension(FileExtension);
 
-            elementHost1.Child = wpfEditor;
+            var elementHost = new ElementHost
+                              {
+                                  Child = wpfEditor,
+                                  Dock = DockStyle.Fill
+                              };
+            this.Controls.Add(elementHost);
         }
 
-        /// <summary>
-        ///     Windows Forms editor
-        /// </summary>
-        private void InitWfEditor()
-        {
-            var wfEditor = new ICSharpCode.TextEditor.TextEditorControl();
-            wfEditor.Dock = DockStyle.Fill;
-            wfEditor.LoadFile(FilePath, true, true);
-            splitContainer1.Panel2.Controls.Add(wfEditor);
-        }
+#endif
     }
 }
