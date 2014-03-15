d334636dfe17857994a292d55b96f4b6ea29188e
Andrew C. Dvorak <acdvorak@gmail.com>
2014-03-15 18:22:00 -0500

    Added MouseActivate.cs to NativeAPI

---
+++ src/Libraries/NativeAPI/Win/User/MouseActivate.cs
@@ -0,0 +1,31 @@
+﻿// ReSharper disable InconsistentNaming
+namespace NativeAPI.Win.User
+{
+    /// <summary>
+    ///     Static class containing values that specify whether the window should be activated and whether the identifier of the mouse message should be discarded
+    ///     in response to a <see cref="WindowMessageType.WM_MOUSEACTIVATE"/> message.
+    /// </summary>
+    public static class MouseActivate
+    {
+        /// <summary>
+        ///     Activates the window, and does not discard the mouse message.
+        /// </summary>
+        public const int MA_ACTIVATE = 1;
+
+        /// <summary>
+        ///     Activates the window, and discards the mouse message.
+        /// </summary>
+        public const int MA_ACTIVATEANDEAT = 2;
+
+        /// <summary>
+        ///     Does not activate the window, and does not discard the mouse message.
+        /// </summary>
+        public const int MA_NOACTIVATE = 3;
+
+        /// <summary>
+        ///     Does not activate the window, but discards the mouse message.
+        /// </summary>
+        public const int MA_NOACTIVATEANDEAT = 4;
+
+    }
+}
