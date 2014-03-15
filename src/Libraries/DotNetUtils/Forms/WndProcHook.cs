528c6bbadada3a2e0b0cb11085c8c75bfeb43c9d
Andrew C. Dvorak <acdvorak@gmail.com>
2014-03-15 13:09:00 -0500

    Refactored WndProcEventHandler out into separate file

--- src/Libraries/DotNetUtils/Forms/WndProcHook.cs
+++ src/Libraries/DotNetUtils/Forms/WndProcHook.cs
@@ -68,15 +68,4 @@ namespace DotNetUtils.Forms
             AssignHandle(_control.Handle);
         }
     }
-
-    /// <summary>
-    ///     Invoked whenever the hooked control's <see cref="Control.WndProc"/> method is called.
-    /// </summary>
-    /// <param name="m">
-    ///     Native window message.
-    /// </param>
-    /// <param name="args">
-    ///     Event data.
-    /// </param>
-    public delegate void WndProcEventHandler(ref Message m, HandledEventArgs args);
 }
