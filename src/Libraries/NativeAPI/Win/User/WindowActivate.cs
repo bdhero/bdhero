d6677ee1aa7b210d47078c7c6f852cfdf4ec8a6c
Andrew C. Dvorak <acdvorak@gmail.com>
2014-03-16 15:39:51 -0500

    Added WindowActivate.cs

---
+++ src/Libraries/NativeAPI/Win/User/WindowActivate.cs
@@ -0,0 +1,25 @@
+﻿// ReSharper disable InconsistentNaming
+namespace NativeAPI.Win.User
+{
+    /// <summary>
+    ///     Static class containing values for the WPARAM field of <see cref="WindowMessageType.WM_ACTIVATE"/> messages.
+    /// </summary>
+    public static class WindowActivate
+    {
+        /// <summary>
+        ///     Activated by some method other than a mouse click (for example, by a call to the SetActiveWindow function or by use of the keyboard interface to select the window).
+        /// </summary>
+        private const int WA_ACTIVE = 1;
+
+        /// <summary>
+        ///     Activated by a mouse click.
+        /// </summary>
+        private const int WA_CLICKACTIVE = 2;
+
+        /// <summary>
+        ///     Deactivated.
+        /// </summary>
+        private const int WA_INACTIVE = 0;
+
+    }
+}
