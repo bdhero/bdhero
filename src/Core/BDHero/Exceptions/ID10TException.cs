a9939849ac1d62d33f284e9ef06ccf99e69355bf
Andrew C. Dvorak <acdvorak@gmail.com>
2014-03-22 23:04:43 -0500

    Added ID10TException

---
+++ src/Core/BDHero/Exceptions/ID10TException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BDHero.Exceptions
+{
+    public class ID10TException : Exception
+    {
+        public ID10TException()
+        {
+        }
+
+        public ID10TException(string message)
+            : base(message)
+        {
+        }
+
+        public ID10TException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
