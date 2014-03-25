76e79fc94ae1bfcb71f17a807d2f4e12966b6c18
Andrew C. Dvorak <acdvorak@gmail.com>
2014-03-25 08:47:27 -0500

    Removed unnecessary [JsonProperty] attributes from UpdateResponse

--- src/Core/UpdateLib/UpdateResponse.cs
+++ src/Core/UpdateLib/UpdateResponse.cs
@@ -24,24 +24,19 @@ namespace UpdateLib
 {
     public class UpdateResponse
     {
-        [JsonProperty(PropertyName = "version")]
         [JsonConverter(typeof(VersionConverter))]
         public Version Version { get; set; }
 
         /// <summary>
         /// ISO 8601 date format (e.g. <c>2008-04-12T12:53Z</c>).  See <see cref="IsoDateTimeConverter"/>.
         /// </summary>
-        [JsonProperty(PropertyName = "date")]
         [JsonConverter(typeof(IsoDateTimeConverter))]
         public DateTime Date { get; set; }
 
-        [JsonProperty(PropertyName = "mirrors")]
         public List<string> Mirrors { get; set; }
 
-        [JsonProperty(PropertyName = "platforms")]
         public PlatformList Platforms { get; set; }
 
-        [JsonProperty(PropertyName = "releaseNotes")]
         public string ReleaseNotes { get; set; }
 
         public UpdateResponse()
@@ -56,15 +51,12 @@ namespace UpdateLib
 
     public class PlatformList
     {
-        [JsonProperty(PropertyName = "windows")]
         public Platform Windows { get; set; }
 
         [JsonIgnore]
-        [JsonProperty(PropertyName = "mac")]
         public Platform Mac { get; set; }
 
         [JsonIgnore]
-        [JsonProperty(PropertyName = "linux")]
         public Platform Linux { get; set; }
 
         public PlatformList()
@@ -77,7 +69,6 @@ namespace UpdateLib
 
     public class Platform
     {
-        [JsonProperty(PropertyName = "packages")]
         public PackageList Packages { get; set; }
 
         public Platform()
@@ -88,16 +79,7 @@ namespace UpdateLib
 
     public class PackageList
     {
-        [JsonProperty(PropertyName = "setup")]
         public Package Setup { get; set; }
-
-        [JsonProperty(PropertyName = "sfx")]
-        public Package Sfx { get; set; }
-
-        [JsonProperty(PropertyName = "sevenZip")]
-        public Package SevenZip { get; set; }
-
-        [JsonProperty(PropertyName = "zip")]
         public Package Zip { get; set; }
     }
 
@@ -106,10 +88,7 @@ namespace UpdateLib
         [JsonProperty(PropertyName = "filename")]
         public string FileName { get; set; }
 
-        [JsonProperty(PropertyName = "sha1")]
         public string SHA1 { get; set; }
-
-        [JsonProperty(PropertyName = "size")]
         public long Size { get; set; }
     }
 }
