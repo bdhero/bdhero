cfd5fd6a3bc102171a3d503305039f9e3e704f22
Andrew C. Dvorak <acdvorak@gmail.com>
2014-03-31 12:13:40 -0500

    Added UpdaterV2 + unit tests

---
+++ src/Core/UpdateLib/V2/UpdaterV2.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using DotNetUtils;
+using RestSharp;
+
+namespace UpdateLib.V2
+{
+    public class UpdaterV2
+    {
+//        private const string DefaultUpdateManifestBaseUrl = "http://update.bdhero.org";
+        private const string DefaultUpdateManifestBaseUrl = "http://192.168.0.151:8080";
+        private const string DefaultUpdateManifestFileUrl = "/update.json";
+
+        #region Public properties
+
+        public string UpdateManifestBaseUrl { get; set; }
+
+        public string UpdateManifestFileUrl { get; set; }
+
+        public Version CurrentVersion { get; set; }
+
+        public bool IsPortable { get; set; }
+
+        public Update LatestUpdate { get; private set; }
+
+        public bool IsUpdateAvailable { get { return LatestUpdate != null && LatestUpdate.Version > CurrentVersion; } }
+
+        #endregion
+
+        #region Public events
+
+        public event UpdaterV2CheckingForUpdateEventHandler Checking;
+
+        public event UpdaterV2UpdateFoundEventHandler UpdateFound;
+
+        public event UpdaterV2UpdateNotFoundEventHandler UpdateNotFound;
+
+        public event UpdaterV2ErrorCheckingForUpdateEventHandler Error;
+
+        #endregion
+
+        private readonly ManualResetEventSlim _isChecking = new ManualResetEventSlim();
+
+        public UpdaterV2()
+        {
+            UpdateManifestBaseUrl = DefaultUpdateManifestBaseUrl;
+            UpdateManifestFileUrl = DefaultUpdateManifestFileUrl;
+            CurrentVersion = new Version();
+            IsPortable = false;
+        }
+
+        public void CheckForUpdateAsync()
+        {
+            // Already checking for an update
+            if (_isChecking.IsSet)
+                return;
+
+            var client = new RestClient(UpdateManifestBaseUrl);
+            var request = new RestRequest(UpdateManifestFileUrl);
+            var asyncRequestHandle = client.ExecuteAsync(request, OnResponse);
+
+            if (Checking != null)
+                Checking(this);
+
+            _isChecking.Set();
+        }
+
+        // TODO: UI callback thread?
+        private void OnResponse(IRestResponse response, RestRequestAsyncHandle asyncHandle)
+        {
+            _isChecking.Wait(); // TODO: Add timeout?
+
+            try
+            {
+                if (response.StatusCode == HttpStatusCode.OK && response.ErrorException == null)
+                {
+                    HandleSuccess(response);
+                }
+                else
+                {
+                    HandleError(response);
+                }
+            }
+            finally
+            {
+                _isChecking.Reset();
+            }
+        }
+
+        private void HandleError(IRestResponse response)
+        {
+            if (Error != null)
+                Error(this, response.ErrorException);
+        }
+
+        private void HandleSuccess(IRestResponse response)
+        {
+            var updateResponse = SmartJsonConvert.DeserializeObject<UpdateResponse>(response.Content);
+
+            LatestUpdate = Update.FromResponse(updateResponse, IsPortable);
+
+            if (IsUpdateAvailable)
+            {
+                if (UpdateFound != null)
+                    UpdateFound(this);
+            }
+            else
+            {
+                if (UpdateNotFound != null)
+                    UpdateNotFound(this);
+            }
+        }
+    }
+
+    public delegate void UpdaterV2CheckingForUpdateEventHandler(UpdaterV2 updater);
+    public delegate void UpdaterV2UpdateFoundEventHandler(UpdaterV2 updater);
+    public delegate void UpdaterV2UpdateNotFoundEventHandler(UpdaterV2 updater);
+    public delegate void UpdaterV2ErrorCheckingForUpdateEventHandler(UpdaterV2 updater, Exception exception);
+}
---
+++ src/Tests/Unit/UpdaterTests/UpdaterV2Test.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using BDHero.Startup;
+using NUnit.Framework;
+using Ninject;
+using Ninject.Activation;
+using Ninject.Modules;
+using UpdateLib;
+using log4net;
+using UpdateLib.V2;
+
+namespace UpdaterTests
+{
+    [TestFixture]
+    public class UpdaterV2Test
+    {
+        private UpdaterV2 _updater;
+        private ManualResetEventSlim _barrier;
+        private string _events;
+
+        [SetUp]
+        public void SetUpClient()
+        {
+            _updater = new UpdaterV2();
+            _barrier = new ManualResetEventSlim();
+            _events = "";
+
+            _updater.Checking += updater => _events += "Checking;";
+            _updater.UpdateFound += updater => _events += "UpdateFound;";
+            _updater.UpdateNotFound += updater => _events += "UpdateNotFound;";
+            _updater.Error += (updater, exception) => _events += "Error;";
+
+            _updater.UpdateFound += updater => _barrier.Set();
+            _updater.UpdateNotFound += updater => _barrier.Set();
+            _updater.Error += (updater, exception) => _barrier.Set();
+        }
+
+        [Test]
+        public void TestUpdateFound()
+        {
+            _updater.CurrentVersion = new Version(0, 0, 0, 0);
+            _updater.IsPortable = true;
+
+            _updater.CheckForUpdateAsync();
+
+            var gotResponse = _barrier.Wait(TimeSpan.FromSeconds(5));
+
+            Assert.IsTrue(gotResponse, "Did not receive a response from the update server within 5 seconds");
+            Assert.IsNotNull(_updater.LatestUpdate);
+            Assert.IsTrue(_updater.IsUpdateAvailable, "Expected an update to be available");
+            Assert.Greater(_updater.LatestUpdate.Version, _updater.CurrentVersion, "LatestUpdate.Version should be greater than CurrentVersion");
+            Assert.AreEqual("Checking;UpdateFound;", _events);
+            Assert.IsTrue(_updater.LatestUpdate.FileName.EndsWith(".zip"), "LatestUpdate.FileName should end with \".zip\"");
+        }
+
+        [Test]
+        public void TestUpdateNotFound()
+        {
+            _updater.CurrentVersion = new Version(999, 0, 0, 0);
+            _updater.IsPortable = true;
+
+            _updater.CheckForUpdateAsync();
+
+            var gotResponse = _barrier.Wait(TimeSpan.FromSeconds(5));
+
+            Assert.IsTrue(gotResponse, "Did not receive a response from the update server within 5 seconds");
+            Assert.IsNotNull(_updater.LatestUpdate);
+            Assert.IsFalse(_updater.IsUpdateAvailable, "Expected no update to be available");
+            Assert.AreEqual("Checking;UpdateNotFound;", _events);
+        }
+
+        [Test]
+        public void TestError()
+        {
+            _updater.CurrentVersion = new Version(0, 0, 0, 0);
+            _updater.IsPortable = true;
+            _updater.UpdateManifestFileUrl += "_404";
+
+            _updater.CheckForUpdateAsync();
+
+            var gotResponse = _barrier.Wait(TimeSpan.FromSeconds(5));
+
+            Assert.IsTrue(gotResponse, "Did not receive a response from the update server within 5 seconds");
+            Assert.IsNull(_updater.LatestUpdate);
+            Assert.IsFalse(_updater.IsUpdateAvailable, "Expected no update to be available");
+            Assert.AreEqual("Checking;Error;", _events);
+        }
+
+        // TODO: Test exception throwing
+    }
+}
