b65c476c0480ed5cfd1ada371751e7768431908c
Andrew C. Dvorak <acdvorak@gmail.com>
2014-03-31 12:13:12 -0500

    Minor - Added JsonNullTest to UpdaterTests

---
+++ src/Tests/Unit/UpdaterTests/JsonNullTest.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace UpdaterTests
+{
+    [TestFixture]
+    public class JsonNullTest
+    {
+        [Test]
+        public void TestEmpty()
+        {
+            var parent = JsonConvert.DeserializeObject<JsonNullTestParentEmpty>("{}");
+            Assert.IsNull(parent.Child);
+        }
+
+        [Test]
+        public void TestPopulated()
+        {
+            var parent = JsonConvert.DeserializeObject<JsonNullTestParentPopulated>("{}");
+            Assert.IsNotNull(parent.Child);
+        }
+    }
+
+    class JsonNullTestParentEmpty
+    {
+        public JsonNullTestChild Child { get; set; }
+    }
+
+    class JsonNullTestParentPopulated
+    {
+        public JsonNullTestChild Child { get; set; }
+
+        public JsonNullTestParentPopulated()
+        {
+            Child = new JsonNullTestChild { Name = "Test" };
+        }
+    }
+
+    class JsonNullTestChild
+    {
+        public string Name { get; set; }
+    }
+}
