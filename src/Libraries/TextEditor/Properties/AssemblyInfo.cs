a10a03b6c4a31411f010cbcf0aa5a318e8817152
Andrew C. Dvorak <acdvorak@gmail.com>
2014-03-31 15:52:11 -0500

    Minor - Added name to AssemblyCopyright attribute in TextEditor and UILib

--- src/Libraries/TextEditor/Properties/AssemblyInfo.cs
+++ src/Libraries/TextEditor/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TextEditor")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © 2014 Andrew C. Dvorak")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
