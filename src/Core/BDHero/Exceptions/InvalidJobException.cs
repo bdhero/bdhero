8ccd32cd0fab7a58a11edc7eec7bdf146fc18362
Andrew C. Dvorak <acdvorak@gmail.com>
2014-04-18 15:08:09 -0500

    Added InvalidJobException and InvalidPlaylistException classes to BDHero project

---
+++ src/Core/BDHero/Exceptions/InvalidJobException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DotNetUtils.Exceptions;
+
+namespace BDHero.Exceptions
+{
+    public class InvalidJobException : ID10TException
+    {
+        public InvalidJobException()
+        {
+        }
+
+        public InvalidJobException(string message)
+            : base(message)
+        {
+        }
+
+        public InvalidJobException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ src/Core/BDHero/Exceptions/InvalidPlaylistException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DotNetUtils.Exceptions;
+
+namespace BDHero.Exceptions
+{
+    public class InvalidPlaylistException : ID10TException
+    {
+        public InvalidPlaylistException()
+        {
+        }
+
+        public InvalidPlaylistException(string message)
+            : base(message)
+        {
+        }
+
+        public InvalidPlaylistException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
