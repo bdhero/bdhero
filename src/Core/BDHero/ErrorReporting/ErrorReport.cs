bf63f906c76d95abd833c937f30a50355da03caa
Andrew C. Dvorak <acdvorak@gmail.com>
2014-04-21 20:24:39 -0500

    Don't redact file/directory names - only redact the parent directory path

--- src/Core/BDHero/ErrorReporting/ErrorReport.cs
+++ src/Core/BDHero/ErrorReporting/ErrorReport.cs
@@ -284,18 +284,8 @@ System Info
         {
             var path = match.Value;
             var name = Path.GetFileName(path);
-            var ext = Path.GetExtension(path);
-            
-            // Directory
-            if (string.IsNullOrEmpty(ext))
-                return match.Result(Redacted);
-
-            // C# source file from a stack trace
-            if (".cs".Equals(ext, StringComparison.InvariantCultureIgnoreCase))
-                return string.Format("{0}{1}{2}", Redacted, Path.DirectorySeparatorChar, name);
-
-            // All other files
-            return match.Result(string.Format("{0}{1}", Redacted, ext));
+            var sep = Path.DirectorySeparatorChar;
+            return string.Format("{0}{1}{2}", Redacted, sep, name);
         }
     }
 }
