e93b214b6b1ec80154d8352e434167653c389398
Andrew C. Dvorak <acdvorak@gmail.com>
2014-04-22 01:47:36 -0500

    Added ObjectExtensions.cs with ToJsonPlainIndented() method to DotNetUtils

---
+++ src/Libraries/DotNetUtils/Extensions/ObjectExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace DotNetUtils.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static string ToJsonPlainIndented(this Object obj)
+        {
+            return JsonConvert.SerializeObject(obj, Formatting.Indented);
+        }
+    }
+}
